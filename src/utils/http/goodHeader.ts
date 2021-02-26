eb478de2319849a1f876acf67fbc31de49ae8bd0
jjs <jjs@foxmail.com>
2021-02-26 23:09:38 +0800

    search logo and two more interface

---
+++ src/utils/http/goodHeader.ts
@@ -0,0 +1,9 @@
+import { httpGet } from '@/utils/http/main';
+import { GoodProp } from '@/utils/http/goodList'
+
+export async function getHeader():Promise<GoodProp[]>{
+    return await httpGet<undefined,GoodProp[]>(
+        "/good/header",
+        undefined
+    )
+}
---
+++ src/utils/http/goodType.ts
@@ -0,0 +1,16 @@
+import { httpGet } from '@/utils/http/main';
+import { GoodProp } from '@/utils/http/goodList'
+
+
+export async function searchByType(
+    type:string,
+    pageSize:number,
+    pageNum:number,
+    orderRule:'ASC'|'DESC',
+    orderBY:'price',
+):Promise<GoodProp[]>{
+    return await httpGet<undefined,GoodProp[]>(
+        `/good/type?type=${type}&pageSize=${pageSize}&pageNum=${pageNum}&orderRule=${orderRule}&orderBY=${orderBY}`,
+        undefined
+    )
+}
